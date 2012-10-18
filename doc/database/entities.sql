7ffb80b4bbb57820e72d4dcc0612a53fe97f7d1b
Igor Ribeiro Lima <igor.lima@lemaf.ufla.br>
2012-10-18 02:07:08 -0300

    Criacao de scripts

---
+++ doc/database/entities.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS siscob.usuario;
+CREATE TABLE siscob.usuario
+  (
+    ID              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NOME            VARCHAR(256) NOT NULL,
+    ENDERECO        VARCHAR(256),
+    CPF             VARCHAR(11),
+    TELEFONE        VARCHAR(10),
+    DATAPENALIZACAO DATE
+  )
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1;
+  
+DROP TABLE IF EXISTS siscob.publicacao;
+CREATE TABLE siscob.publicacao
+  (
+    ID              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    TITULO          VARCHAR(256) NOT NULL,
+    EDITORA         VARCHAR(256),
+    ANO             VARCHAR(11),
+    TIPO            VARCHAR(10)
+  )
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS siscob.periodico;
+CREATE TABLE siscob.periodico
+  (
+    ID              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NUMEDICAO       INT,
+    MES             VARCHAR(256)
+  )
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS siscob.livro;
+CREATE TABLE siscob.livro
+  (
+    ID                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    AUTORES           VARCHAR(256),
+    QTDEXEMPLARES     INT,
+    ID_ITEMEMPRESTIMO INT
+  )
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS siscob.itememprestimo;
+CREATE TABLE siscob.itememprestimo
+  (
+    ID              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ID_PUBLICACAO   INT,
+    ID_EMPRESTIMO   INT
+  )
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS siscob.emprestimo;
+CREATE TABLE siscob.emprestimo
+  (
+    ID              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    DATAEMPRESTIMO  DATE,
+    DATADEVOLUCAO   DATE,
+    ID_USUARIO      INT
+  )
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1;
