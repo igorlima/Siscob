db4d4932d173436e58576b8275d998adf71cb55b
Igor Ribeiro Lima <igor.lima@lemaf.ufla.br>
2012-10-18 01:30:24 -0300

    Estrutura de navegacao da tela principal

--- java/src/main/webapp/js/app.js
+++ java/src/main/webapp/js/app.js
@@ -2,9 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('siscob', ['siscob.filters', 'siscob.services', 'siscob.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/usuario', {templateUrl: 'partials/usuario.html', controller: MyCtrl1});
+    $routeProvider.when('/publicacao', {templateUrl: 'partials/publicacao.html', controller: MyCtrl2});
+    $routeProvider.otherwise({redirectTo: '/siscob'});
   }]);
