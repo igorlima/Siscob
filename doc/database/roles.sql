e8f033999202fc8f27725d449409b467155af391
Igor Ribeiro Lima <igor.lima@lemaf.ufla.br>
2012-10-20 22:30:16 -0300

    utilizando banco de dados local

--- doc/database/roles.sql
+++ doc/database/roles.sql
@@ -16,3 +16,13 @@ CREATE TABLE siscob.user_roles
   )
 ENGINE=InnoDB
 DEFAULT CHARSET=latin1;
+
+INSERT INTO `siscob`.`users` (`USER_NAME`,`USER_PASS`)
+       VALUES("cadu","123");
+
+INSERT INTO `siscob`.`user_roles` (`USER_NAME`,`ROLE_NAME`,`APPLICATION`)
+       VALUES ("cadu","SISCOB+PERMISSAO+usuario+VER","SISCOB");
+INSERT INTO `siscob`.`user_roles` (`USER_NAME`,`ROLE_NAME`,`APPLICATION`)
+       VALUES ("cadu","SISCOB+PERMISSAO+usuario+EDITAR","SISCOB");
+INSERT INTO `siscob`.`user_roles` (`USER_NAME`,`ROLE_NAME`,`APPLICATION`)
+       VALUES ("cadu","GRUPO+Membros","SISCOB");
